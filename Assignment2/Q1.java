309f1fe89407857069c616d45c26238a44e2d51a
jjeni <jenijofficial@gmail.com>
2025-07-20 23:31:24 +0530

    Added Assignment 2 folder and solved Q1

---
+++ Assignment2/Q1.java
@@ -0,0 +1,30 @@
+//1. Write a program that uses if statement to find the minimum of three numbers.
+
+package Assignment2;
+
+import java.util.Scanner;
+
+public class Q1 {
+
+    public static int minOfThree(int a, int b, int c){
+        int m = b;
+        if (a<b){
+            m = a;
+        }
+        if (c<m){
+            m = c;
+        }
+        return m;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int a = sc.nextInt();
+        System.out.println("Enter second number:");
+        int b = sc.nextInt();
+        System.out.println("Enter third number:");
+        int c = sc.nextInt();
+        System.out.println("Minimum is : " + minOfThree(a, b, c));
+        sc.close();
+    }
+}
